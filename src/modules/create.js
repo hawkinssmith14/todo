ceade279730d5b2bd55042c42a1431b1a2542338
Hawkins Smith <hawkins.smith0@gmail.com>
2026-03-08 16:02:44 +1100

    More shit

--- src/modules/create.js
+++ src/modules/create.js
@@ -1,11 +1,17 @@
 import "../styles.css";
 
 class Item {
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, dueDate, priority, status = "Todo", project) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+        this.status = status;
+        this.project = project;
+    }
+
+    completeItem() {
+        this.status = "Done";
     }
 }
 
@@ -15,17 +21,34 @@ class Project {
         this.items = [];
     }
 
-    addItemToProject(item) {
+    createItem(title, description, dueDate, priority) {
+        const item = new Item(title, description, dueDate, priority, this);
         this.items.push(item);
     }
 }
 
-const defaultProject = new Project("Default");
-const example1 = new Item("Example item 1", "This is an example item", "08/03/2026", "High");
-const example2 = new Item("Example item 2", "This is an example item", "08/03/2026", "High");
-const example3 = new Item("Example item 3", "This is an example item", "08/03/2026", "High");
-defaultProject.addItemToProject(example1);
-defaultProject.addItemToProject(example2);
-defaultProject.addItemToProject(example3);
 
-console.log(defaultProject.items);
+const defaultProject = new Project("Default");
+defaultProject.createItem("Example item 0", "This is an example item", "08/03/2026", "High");
+
+defaultProject.items.forEach(item => {
+    console.log(`${item.title} is due on ${item.dueDate} | Priority: ${item.priority} | Status: ${item.status}`);
+});
+
+const projects = [defaultProject];
+
+
+const content = document.querySelector(".content");
+projects.forEach(project => {
+    const section = document.createElement("div");
+    section.classList.add("card");
+    section.textContent = `${project.title}`;
+    content.appendChild(section);
+
+    project.items.forEach(item => {
+        const itemName = document.createElement("p");
+        itemName.textContent = `${item.title}`;
+        section.appendChild(itemName);  
+    })
+})
+
