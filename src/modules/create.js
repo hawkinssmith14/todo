ab8c5e8146dea0dd0b2d1009c82cf33ba8f252f8
Hawkins Smith <hawkins.smith0@gmail.com>
2026-03-08 11:07:07 +1100

    Added ability to create item and project, and add item to a project

--- src/modules/create.js
+++ src/modules/create.js
@@ -1,1 +1,31 @@
 import "../styles.css";
+
+class Item {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+class Project {
+    constructor(title) {
+        this.title = title;
+        this.items = [];
+    }
+
+    addItemToProject(item) {
+        this.items.push(item);
+    }
+}
+
+const defaultProject = new Project("Default");
+const example1 = new Item("Example item 1", "This is an example item", "08/03/2026", "High");
+const example2 = new Item("Example item 2", "This is an example item", "08/03/2026", "High");
+const example3 = new Item("Example item 3", "This is an example item", "08/03/2026", "High");
+defaultProject.addItemToProject(example1);
+defaultProject.addItemToProject(example2);
+defaultProject.addItemToProject(example3);
+
+console.log(defaultProject.items);
