27a3aee06a86560f076f5f7c4b5f490b6c135757
Hawkins Smith <hawkins.smith0@gmail.com>
2026-03-09 18:03:10 +1100

    Everything mostly working

--- src/modules/create.js
+++ src/modules/create.js
@@ -1,124 +1,122 @@
 import "../styles.css";
 
 class Item {
-    constructor(title, description, dueDate, priority, status = "Todo", project) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.status = status;
-        this.project = project;
-    }
+  constructor(title, description, dueDate, priority, status = "Todo", project) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = status;
+    this.project = project;
+  }
 
-    completeItem() {
-        this.status = "Done";
-    }
+  completeItem() {
+    this.status = "Done";
+  }
 }
 
 class Project {
-    constructor(title) {
-        this.title = title;
-        this.items = [];
-    }
+  constructor(title) {
+    this.title = title;
+    this.items = [];
+  }
 
-    createItem(title, description, dueDate, priority) {
-        const item = new Item(title, description, dueDate, priority, this);
-        this.items.push(item);
-        return item;
-    }
+  createItem(title, description, dueDate, priority) {
+    const item = new Item(title, description, dueDate, priority, this);
+    this.items.push(item);
+    return item;
+  }
 }
 
 const defaultProject = new Project("Default Project");
-defaultProject.createItem("Example item 0", "This is an example item", "08/03/2026", "High");
+defaultProject.createItem(
+  "Example item 0",
+  "This is an example item",
+  "08/03/2026",
+  "High"
+);
 
-defaultProject.items.forEach(item => {
-    console.log(`${item.title} is due on ${item.dueDate} | Priority: ${item.priority} | Status: ${item.status}`);
+defaultProject.items.forEach((item) => {
+  console.log(
+    `${item.title} is due on ${item.dueDate} | Priority: ${item.priority} | Status: ${item.status}`
+  );
 });
 
 const projects = [defaultProject];
 
-
 const content = document.querySelector(".content");
-projects.forEach(project => {
-    renderProject(project);
-})
+projects.forEach((project) => {
+  renderProject(project);
+});
 
-// Project creation button & modal 
+// Project creation button & modal
 
 const createProjectBtn = document.querySelector(".create-project-btn");
 createProjectBtn.addEventListener("click", () => {
-    const projectCreationModal = document.createElement("div");
-    projectCreationModal.classList.add("project-creation-modal");
-    const projectNameInput = document.createElement("input");
-    const doneBtn = document.createElement("button");
-    doneBtn.textContent = "Done";
-    const cancelBtn = document.createElement("button");
-    cancelBtn.textContent =  "Cancel";
-    projectCreationModal.appendChild(projectNameInput);
-    projectCreationModal.appendChild(doneBtn);
-    projectCreationModal.appendChild(cancelBtn);
-    content.appendChild(projectCreationModal);
+  const projectCreationModal = document.createElement("div");
+  projectCreationModal.classList.add("project-creation-modal");
+  const projectNameInput = document.createElement("input");
+  const doneBtn = document.createElement("button");
+  doneBtn.textContent = "Done";
+  const cancelBtn = document.createElement("button");
+  cancelBtn.textContent = "Cancel";
+  projectCreationModal.appendChild(projectNameInput);
+  projectCreationModal.appendChild(doneBtn);
+  projectCreationModal.appendChild(cancelBtn);
+  content.appendChild(projectCreationModal);
 
-    doneBtn.addEventListener("click", () => {
-        const project = new Project(projectNameInput.value);
-        projects.push(project);
-        renderProject(project);
-        projectCreationModal.remove();
-    })    
+  doneBtn.addEventListener("click", () => {
+    const project = new Project(projectNameInput.value);
+    projects.push(project);
+    renderProject(project);
+    projectCreationModal.remove();
+  });
 
-    cancelBtn.addEventListener("click", () => {
-        projectCreationModal.remove();
-    })
-})
+  cancelBtn.addEventListener("click", () => {
+    projectCreationModal.remove();
+  });
+});
 
 function renderProject(project) {
-    const section = document.createElement("div");
-    const itemContainer = document.createElement("div");
-    const projectTitle = document.createElement("h3");
-    projectTitle.textContent = `${project.title}`;
-    section.classList.add("card");
-    section.appendChild(projectTitle);
-    content.appendChild(section);
-    section.appendChild(itemContainer);
+  const section = document.createElement("div");
+  const itemContainer = document.createElement("div");
+  const projectTitle = document.createElement("h3");
+  projectTitle.textContent = `${project.title}`;
+  section.classList.add("card");
+  section.appendChild(projectTitle);
+  content.appendChild(section);
+  section.appendChild(itemContainer);
 
-    project.items.forEach(item => {
-        renderItem(item, itemContainer); 
-    })
+  project.items.forEach((item) => {
+    renderItem(item, itemContainer);
+  });
 
-    const createItemBtn = document.createElement("button");
-    createItemBtn.textContent = `Create Item`;
-    section.appendChild(createItemBtn);
+  const createItemBtn = document.createElement("button");
+  createItemBtn.textContent = `Create Item`;
+  section.appendChild(createItemBtn);
 
-    createItemBtn.addEventListener("click", () => {
-        const itemCreationModal = document.createElement("div");
-        const itemNameInput = document.createElement("input");
-        const doneBtn = document.createElement("button");
-        const cancelBtn = document.createElement("button");
-        doneBtn.textContent = "Done";
-        cancelBtn.textContent = "Cancel";
-        section.appendChild(itemCreationModal);
-        itemCreationModal.appendChild(itemNameInput);
-        itemCreationModal.appendChild(doneBtn);
-        itemCreationModal.appendChild(cancelBtn);
-        
-        doneBtn.addEventListener("click", () => {
-            const newItem = project.createItem(itemNameInput.value, project);
-            renderItem(newItem, itemContainer);
-            itemCreationModal.remove();
-        })
-    })   
+  createItemBtn.addEventListener("click", () => {
+    const itemCreationModal = document.createElement("div");
+    const itemNameInput = document.createElement("input");
+    const doneBtn = document.createElement("button");
+    const cancelBtn = document.createElement("button");
+    doneBtn.textContent = "Done";
+    cancelBtn.textContent = "Cancel";
+    section.appendChild(itemCreationModal);
+    itemCreationModal.appendChild(itemNameInput);
+    itemCreationModal.appendChild(doneBtn);
+    itemCreationModal.appendChild(cancelBtn);
+
+    doneBtn.addEventListener("click", () => {
+      const newItem = project.createItem(itemNameInput.value, project);
+      renderItem(newItem, itemContainer);
+      itemCreationModal.remove();
+    });
+  });
 }
 
 function renderItem(item, itemContainer) {
-    const itemName = document.createElement("p");
-        itemName.textContent = `${item.title}`;
-        itemContainer.appendChild(itemName); 
+  const itemName = document.createElement("p");
+  itemName.textContent = `${item.title}`;
+  itemContainer.appendChild(itemName);
 }
-
-
-
-
-
-
-
-
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,7 +1,7 @@
 // Need to loop through the project items to display them
 const content = document.querySelector(".content");
-defaultProject.items.forEach(item => {
-    const section = projectName.createElement("h3");
-    content.appendChild(section);
-    projectName.textContent = `${item.title}`;
-})
+defaultProject.items.forEach((item) => {
+  const section = projectName.createElement("h3");
+  content.appendChild(section);
+  projectName.textContent = `${item.title}`;
+});
